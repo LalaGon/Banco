5a563a8bb24942cd40da85bb7e27876e793eb6dd
LalaGon <63089052+LalaGon@users.noreply.github.com>
2020-05-25 21:21:39 -0300

    Add files via upload

---
+++ poo/model/Pessoa.java
@@ -0,0 +1,28 @@
+package com.poo.model;
+
+public class Pessoa {
+	private int id;
+	private String nome;
+	private String sexo;
+	
+	//getters and setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+}
